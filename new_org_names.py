65668c30ad088878c52212a1655553d40687f3a5
Aaron Hu <aaron@ip-192-168-1-30.us-west-2.compute.internal>
2021-06-08 09:55:04 -0700

    add console log to keep track of place in line

---
+++ new_org_names.py
@@ -0,0 +1,9 @@
+# [
+# 	{"org_id": "{{ID of the company}}", "new_org_name": "{{New name of the org}}"}
+# ]
+
+
+orgs = [
+  {"org_id": "261654839", "new_org_name": "new org name1"},
+  {"org_id": "223898395", "new_org_name": "new org name2"}
+  ]
--- script.py
+++ script.py
@@ -1,31 +1,37 @@
 from api_key import *
-from file_ids import *
+from new_org_names import *
 from helper import *
 
 import requests
 import time
 
 
-api_key = 'API KEY'
+log_file = open("log.csv", "w")
+log_file.write("Get org status, Org ID, Org Name, Org Domain, Change org name status, New org ID, New org name, New org domain, Note \n")
 
+total_count = len(orgs)
+print(total_count)
+current_count = 0
 
-entries = [
-  {"opp_id": "2420457", "opp_name": "new opp name"},
-  {"opp_id": "2420468", "opp_name": "new opp name"}
-  ]
-
-for x in entries:
-
-  url = "https://api.affinity.co/opportunities/"
+for org in orgs:
+  current_count = current_count + 1
+  url = "https://api.affinity.co/organizations/"
 
   data = [
-    ('name', x['opp_name']),
+    ('name', org['new_org_name']),
   ]
+  print("Processing " + str(org['org_id']) + " - " + str(current_count) + " / " + str(total_count))
+  # Get org info before change
+  response = requests.get(url + org['org_id'], data=data, auth=('', api_key))
+  log_file.write(str(response.status_code) + ", " + str(response.json()['id']) + ", " + response.json()['name'] + ", " + response.json()['domain'])
 
-  response = requests.put(url+x['opp_id'], data=data, auth=('', api_key))
+  # Update org with new name and log changes
+  response = requests.put(url + org['org_id'], data=data, auth=('', api_key))
+  if response.status_code == 200:
+    log_file.write(", " + "Success" + ", " + str(response.json()['id']) + ", " + response.json()['name'] + ", " + response.json()['domain'] + "\n")
+  else:
+    log_file.write(", " + "Uncessful" + ", " + " " + ", " + " " + ", " + " " + str(response.json() + "\n"))
 
-  json = response.json()
 
-  print(response.status_code)
-  print(json)
-  print(" ")
+log_file.close()
+print("Done!")
